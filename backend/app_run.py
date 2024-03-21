dc154986cba5b88ab8eb5cee259114259b19339d
LangKing9 <wzw09@zju.edu.cn>
2024-03-21 17:44:18 +0800

    feat: umls接口完成，重构环境变量

--- backend/app_run.py
+++ backend/app_run.py
@@ -1,11 +1,18 @@
+import os
 import re
 import json
 import openai
+import sqlite3  
+import requests
+from fuzzywuzzy import process  
 from flask import Flask, jsonify, request, Blueprint, render_template, abort, send_from_directory
+from dotenv import load_dotenv
+load_dotenv()
 
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_base = os.getenv('OPENAI_API_BASE')
+umls_api_key = os.getenv('UMLS_API_KEY')
 
-openai.api_key = "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"  
-openai.api_base = "https://api.jarvis73.com/v1"  
 
 # Initialize the app
 app = Flask(__name__)
@@ -112,6 +119,219 @@ def select_usmle_topic():
 def get_usmle_topics():
     data = json.load(open('database/usmle2022.json', 'r', encoding='utf-8'))
     return jsonify(data)
+
+def get_cui_and_title(content):
+    url = 'https://uts-ws.nlm.nih.gov/rest/search/current'
+    params = {
+        'string': 'Fever',
+        'apiKey': umls_api_key
+    }
+    response = requests.get(url, params=params)
+    search_result = response.json().get('result').get('results')[0]
+    CUI = search_result.get('ui')
+    title = search_result.get('name')
+    return CUI, title
+
+def get_definitions(CUI):
+    third_url = f'https://uts-ws.nlm.nih.gov/rest/content/current/CUI/{CUI}/definitions'
+
+    params = {
+        'apiKey': umls_api_key,
+    }
+
+    response = requests.get(third_url, params=params)
+    priority = ['MSH', 'HPO', 'NCI']  
+      
+    # 用于存储不以"MSH"开头的第一个对象  
+    first_non_msh = None  
+    for obj in response.json().get('result'):  
+        root_source = obj.get('rootSource', '')  
+          
+        if root_source in priority:  
+            return obj.get('value')
+          
+        if not first_non_msh and not root_source.startswith('MSH'):  
+            first_non_msh = obj  
+      
+    return first_non_msh.get('value')
+
+def get_relations(CUI, label):
+    second_url = f'https://uts-ws.nlm.nih.gov/rest/content/current/CUI/{CUI}/relations'
+
+    params = {
+        'apiKey': umls_api_key,
+        'includeAdditionalRelationLabels': label
+    }
+
+    response = requests.get(second_url, params=params)
+    return [item.get('relatedIdName') for item in response.json().get('result')]
+
+def get_symptom_result(content):
+    CUI, title = get_cui_and_title(content)
+
+    ans = {
+        'Symptom': title,
+        'Definitions': get_definitions(CUI),
+        'Relationship': {
+            'clinically_associated_with': get_relations(CUI, 'clinically_associated_with')
+        }
+    }
+    return jsonify(ans)
+
+def get_diagnosis_result(content):
+    CUI, title = get_cui_and_title(content)
+    ans = {
+        'Diagnosis': title,
+        'Definitions': get_definitions(CUI),
+        'Relationship': {
+            'may_be_treated_by': get_relations(CUI, 'may_be_treated_by')
+        }
+    }
+    return jsonify(ans)
+
+def get_drugs_list():  
+    # 连接到SQLite数据库  
+    conn = sqlite3.connect('database/full_database.db')  
+    cursor = conn.cursor()  
+      
+    # 查询所有drugbank_id和name  
+    query = "SELECT drugbank_id, name FROM drugs"  
+    cursor.execute(query)  
+      
+    # 获取所有结果  
+    drugs_list = cursor.fetchall()  
+      
+    # 关闭数据库连接  
+    cursor.close()  
+    conn.close()  
+      
+    return drugs_list  
+  
+def get_best_match_drug_id(content, drugs_list):  
+    # 使用fuzzywuzzy找到最匹配的name  
+    best_match = process.extractOne(content, dict(drugs_list))  
+    # 返回最匹配的drugbank_id  
+    return best_match[2]  # best_match的格式是 (匹配的文本, 匹配得分, 匹配项的索引)  
+  
+def get_full_drug_info(drugbank_id):  
+    # 再次连接数据库，根据drugbank_id获取完整信息  
+    conn = sqlite3.connect('database/full_database.db')  
+    cursor = conn.cursor()  
+      
+    query = "SELECT * FROM drugs WHERE drugbank_id = ?"  
+    cursor.execute(query, (drugbank_id,))  
+      
+    # 获取查询结果  
+    row = cursor.fetchone()  
+      
+    # 关闭数据库连接  
+    cursor.close()  
+    conn.close()  
+      
+    return row  
+
+def get_related_products(drugbank_id):  
+    conn = sqlite3.connect('database/full_database.db')  
+    cursor = conn.cursor()  
+      
+    # 查询与drugbank_id相关的所有产品名称  
+    query = "SELECT DISTINCT name FROM Products WHERE drugbank_id = ?"  
+    cursor.execute(query, (drugbank_id,))  
+      
+    # 获取所有结果，并提取产品名称  
+    products = cursor.fetchall()  
+    product_names = [product[0] for product in products]  
+      
+    cursor.close()  
+    conn.close()  
+      
+    return product_names
+
+def get_food_interaction(drugbank_id):
+    conn = sqlite3.connect('database/full_database.db')
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM FoodInteraction WHERE drugbank_id = ?"
+
+    cursor.execute(query, (drugbank_id,))
+
+    food_interactions = cursor.fetchall()
+    food_interaction_list = '\n'.join([food[1] for food in food_interactions])
+    return food_interaction_list
+
+def get_drug_interaction(drugbank_id):
+    conn = sqlite3.connect('database/full_database.db')
+    cursor = conn.cursor()
+
+    query = "SELECT name_anti, drugInteraction_description FROM DrugInteraction WHERE drugbank_id = ?"
+
+    cursor.execute(query, (drugbank_id,))
+
+    drug_interactions = cursor.fetchall()
+
+    drug_interaction_list = []
+    for drug_interaction in drug_interactions:
+        drug_interaction_dict = {
+            "drug_inter": '',
+            "drug_name": drug_interaction[0],
+            "detail_info": drug_interaction[1]
+        }
+        detail_info = drug_interaction[1].lower()  # 转换为小写，以便于比较  
+        if "risk" in detail_info and "combined with" in detail_info:  
+            drug_interaction_dict['drug_inter'] = 'increasing risk of combined use'  
+        elif "efficacy" in detail_info and "increase" in detail_info:
+            drug_interaction_dict['drug_inter'] = 'increasing efficacy of combined use'  
+        elif "efficacy" in detail_info and "decrease" in detail_info:
+            drug_interaction_dict['drug_inter'] = 'decreasing efficacy of combined use'  
+        elif "increase" in detail_info and "activities" in detail_info:  
+            drug_interaction_dict['drug_inter'] = 'increase activity'  
+        elif "decrease" in detail_info and "activities" in detail_info:  
+            drug_interaction_dict['drug_inter'] = 'decrease activity'  
+        else:  
+            drug_interaction_dict['drug_inter'] = 'unknown'  
+        drug_interaction_list.append(drug_interaction_dict)
+    return drug_interaction_list
+
+
+def get_treatment_result(content):
+    drugs_list = get_drugs_list()
+    best_match_id = get_best_match_drug_id(content, drugs_list)  
+    full_drug_info = get_full_drug_info(best_match_id)  
+
+    ans = {
+        "Treatment": full_drug_info[1],
+        "Definitions": full_drug_info[2],
+        "Indication": full_drug_info[3],
+        "Related_Product": get_related_products(best_match_id),
+        "Adverse_Effects": [],
+        "Food_Interaction": get_food_interaction(best_match_id),
+        "Drug_Interaction": get_drug_interaction(best_match_id)
+    }
+    return jsonify(ans)
+
+def get_complication_result(content):
+    CUI, title = get_cui_and_title(content)
+    ans = {
+        'Complication': title,
+        'Definitions': get_definitions(CUI),
+    }
+    return jsonify(ans)
+
+@app.route('/node', methods=['GET'])
+def get_node():
+    type = request.json.get('type')
+    content = request.json.get('content')
+
+    if type == 'Symptom':
+        return get_symptom_result(content)
+    elif type == 'Diagnosis':
+        return get_diagnosis_result(content)
+    elif type == 'Treatment':
+        return get_treatment_result(content)
+    elif type == 'Complication':
+        return get_complication_result(content)
+    else:
+        return jsonify({"message": "unknown type"})
 
 if __name__ == "__main__":
     # app.register_blueprint(datatone_routes.datatone_bp, url_prefix='/datatone')
